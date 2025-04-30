d206f3595733104b12edf38063fed8f85e435980
Atomica-A <atomicaconnect@gmail.com>
2025-04-30 21:57:44 +0530

    Added some more books for testing

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import sys
 from stats import words_count
 from stats import character_count
 from stats import report
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+
     text = get_book_text(filepath)
     count = words_count(text)
     c_count = character_count(text)
@@ -23,6 +29,5 @@ Found {count} total words
 
 
 main()
-
 
 
