4fad812af6d26a91f4aa8ed03922d3da2cd21ea7
Atomica-A <atomicaconnect@gmail.com>
2025-04-30 21:29:53 +0530

    Report done Frankenstein

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import words_count
+from stats import character_count
+from stats import report
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +10,16 @@ def main():
     filepath = 'books/frankenstein.txt'
     text = get_book_text(filepath)
     count = words_count(text)
-    print(f"{count} words found in the document")
+    c_count = character_count(text)
+    f_report = report(c_count)
+    print(f'''
+============ BOOKBOT ============
+Analyzing book found at {filepath}...
+----------- Word Count ----------
+Found {count} total words
+--------- Character Count -------
+{f_report}
+============= END ===============''')
 
 
 main()
